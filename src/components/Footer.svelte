50f0b6a2caafbc0efd18314efd1976ee2c7be8c4
saadkhatri1 <khatrisaad233@gmail.com>
2025-11-23 19:38:37 +0530

    about, contact page

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,12 +1,7 @@
 <footer class="col-span-2 mt-20">
-  <div class="columns-3">
-    <ul>Links</ul>
-    <ul>some Links</ul>
-    <ul>Some other Links</ul>
-  </div>
   <p>
     All Rights Reserved &copy; <a target="_blank" href="github.com/mr-saad"
-      >Saad Khatri 2025</a
+      >Saad Khatri {new Date().getFullYear()}</a
     >
   </p>
 </footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,4 +33,73 @@
       </a>
     {/each}
   </div>
+  <div class="mt-20">
+    <h4 id="about" class="text-3xl font-bold mb-5 scroll-m-4">About Me</h4>
+    <p class="mb-5">
+      My name is <b>Saad Khatri</b>. A Computer Science student currently
+      pursuing M.Sc. (CA-IT) at Department of Computer Science, Kachchh
+      University. I am working towards becoming a Full Stack Web Developer with
+      a strong interest in <b>modern web technologies</b> (Next.js, Svelte, Astro).
+    </p>
+    <p class="mb-5">
+      I enjoy building <b>fast</b>, user-friendly and scalable web applications.
+      I have experience working with Next.js, Astro, Svelte and backend
+      technologies such as Express.js and MongoDB. I have also worked with
+      Sanity CMS and deployed multiple projects using Vercel.
+    </p>
+    <p class="mb-5">
+      I like learning new tools and improving my skills <b
+        >through real projects</b
+      >. My goal is to contribute to meaningful products while exploring areas
+      like mobile app development and software engineering.
+    </p>
+    <p class="mb-5">
+      Outside of coding, I enjoy design, problem-solving and exploring new ideas
+      that help me grow both technically and creatively.
+    </p>
+  </div>
+  <div class="mt-20">
+    <h4 id="contact" class="text-3xl font-bold mb-5 scroll-m-4">
+      Get in Touch
+    </h4>
+
+    <p class="mb-5">Thank you for visiting my portfolio.</p>
+    <p class="mb-5">
+      If you would like to discuss an opportunity, collaboration or have any
+      questions, feel free to reach out.
+    </p>
+    <p class="mb-2">You can contact me through:</p>
+    <ul class="ml-2 *:mb-2 mb-5">
+      <li>
+        Email: <b
+          ><a href="mailto:khatrisaad233@gmail.com">khatrisaad233@gmail.com</a
+          ></b
+        >
+      </li>
+      <li>Phone: <b><a href="tel:917016039766">+91 7016039766</a></b></li>
+      <li>
+        Resume: <b><a href="/Resume.pdf" target="_blank">Saad Khatri</a></b>
+      </li>
+      <li>
+        GitHub: <b
+          ><a href="https://github.com/mr-saad" target="_blank"
+            >github.com/mr-saad</a
+          ></b
+        >
+      </li>
+      <li>
+        LinkedIn: <b
+          ><a
+            href="https://www.linkedin.com/in/saad-khatri-a60753391/"
+            target="_blank">Saad Khatri</a
+          ></b
+        >
+      </li>
+      <li>Location: <b>Bhuj - Kachchh, Gujarat - India</b></li>
+    </ul>
+    <p>
+      I will do my best to respond as soon as possible. I look forward to
+      connecting with you.
+    </p>
+  </div>
 </div>
